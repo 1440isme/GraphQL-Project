c27c74168f1e7c59db931c1fd3eb90b8b8973e0f
1440isme <23110184@student.hcmute.edu.vn>
2025-09-24 21:20:19 +0700

    commit

---
+++ src/main/java/vn/binh/graphqlproject/entity/Category.java
@@ -0,0 +1,27 @@
+package vn.binh.graphqlproject.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Category")
+public class Category implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long categoryId;
+    private String categoryName;
+    private String images;
+    @JsonIgnore
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL )
+    private Set<Product> products;
+}
